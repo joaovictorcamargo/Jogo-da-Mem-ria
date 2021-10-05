e4244d115d9038f05de07f540519b8bbb1b35e5d
joaovictorcamargo <jc7155004@gmail.com>
2021-10-05 15:02:36 -0300

    Jogo da Memoria

---
+++ scripts/game.js
@@ -0,0 +1,132 @@
+let game = {
+
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    nations: ['ale',
+        'arg',
+        'bra',
+        'chi',
+        'esp',
+        'fra',
+        'ing',
+        'jap',
+        'sui',
+        'usa'],
+
+    cards: null,
+    segundos: 0,
+    minutos: 0,
+    firstMove: true,
+    tries: 0,
+
+    setCard: function (id) {
+
+        let card = this.cards.filter(card => card.id === id)[0];
+
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    checkMatch: function () {
+        this.tries++
+        setTries();
+        if (!this.firstCard || !this.secondCard) {
+            return false
+        } else {
+            return this.firstCard.icon === this.secondCard.icon;
+        }
+    },
+
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    unflipCards: function () {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+    checkGameOver: function () {
+        return this.cards.filter(card => !card.flipped).length == 0
+    },
+
+    createCardsFromNations: function () {
+        this.cards = [];
+
+        this.nations.forEach((nation) => {
+            this.cards.push(this.createPairFromNations(nation));
+        })
+
+        this.cards = this.cards.flatMap(pair => pair);
+        this.shuffleCards()
+
+        return this.cards;
+
+    },
+
+    createPairFromNations: function (nation) {
+
+        return [{
+            id: this.createIdWithNation(nation),
+            icon: nation,
+            flipped: false,
+        }, {
+            id: this.createIdWithNation(nation),
+            icon: nation,
+            flipped: false,
+        }]
+
+    },
+
+    createIdWithNation: function (nation) {
+        return nation + parseInt(Math.random() * 1000)
+    },
+
+    shuffleCards: function (cards) {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
+        }
+
+    },
+
+    timer: function () {
+
+        control = setInterval(() => {
+            if (this.segundos < 60) {
+                this.segundos++
+            } else {
+                this.minutos++;
+                this.segundos = 0;
+            }
+            setClock();
+        }, 1000);
+
+        setClock();
+    }
+
+
+}
---
+++ scripts/script.js
@@ -0,0 +1,132 @@
+const front = "card_front";
+const back = "card_back";
+const card = "card";
+const icon = "icon";
+
+startGame();
+
+function startGame() {
+
+    initializeCards(game.createCardsFromNations());
+
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+    gameBoard.innerHTML = "";
+    game.cards.forEach(card => {
+
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add('card');
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement);
+
+    })
+
+}
+
+function createCardContent(card, cardElement) {
+
+    createCardFace(front, card, cardElement);
+    createCardFace(back, card, cardElement)
+
+}
+
+function createCardFace(face, card, element) {
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if (face === front) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(icon);
+        iconElement.src = "./assets/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        let iconElement2 = document.createElement('img');
+        iconElement2.src = "./assets/images/bra.png";
+        cardElementFace.appendChild(iconElement2);
+    }
+
+    element.appendChild(cardElementFace);
+
+}
+
+function flipCard() {
+
+    if (game.firstMove) {
+        game.timer();
+        game.firstMove = false;
+    }
+
+    if (game.setCard(this.id)) {
+
+        this.classList.add("flip");
+
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+                if (game.checkGameOver()) {
+                    clearInterval(control);
+                    let gameOverLayer = document.getElementById("gameOver")
+                    gameOverLayer.style.display = 'flex';
+                }
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+            };
+        }
+
+    }
+
+}
+
+function restart() {
+    startGame();
+    game.minutos = 0;
+    game.segundos = 0;
+    game.tries = 0;
+    setClock();
+    setTries();
+    let gameOverLayer = document.getElementById("gameOver")
+    gameOverLayer.style.display = 'none';
+}
+
+function setClock() {
+    let secondsTag = document.getElementById("seconds");
+    let minutesTag = document.getElementById("minutes");
+
+    if (game.segundos < 10) {
+        secondsTag.innerText = `:0${game.segundos}`;
+    } else {
+        secondsTag.innerText = `:${game.segundos}`;
+    }
+
+    if (game.minutos < 10) {
+        minutesTag.innerText = `0${game.minutos}`
+    } else {
+        minutesTag.innerText = `${game.minutos}`
+    }
+
+}
+
+function setTries() {
+    let tries = document.getElementById("try");
+
+    if (game.tries < 10) {
+        tries.innerHTML = `0${game.tries}`;
+    } else {
+        tries.innerHTML = `${game.tries}`;
+    }
+}
